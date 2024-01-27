c9d37b093fcf90878512612a6ee5a65812655f60
Leon Björklund <leon.bjorklund@gmail.com>
2024-01-27 11:59:28 +0100

    form added

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -8,12 +8,13 @@ import { HousingLocation } from "../housing-location";
   standalone: true,
   imports: [CommonModule, RouterModule],
   template: `
-    <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}" />
-      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
-    </section>
-    <a [routerLink]="['/details', housingLocation.id]">Learn More!</a>
+    <a [routerLink]="['/details', housingLocation.id]" class="listing-link">
+      <section class="listing">
+        <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}" />
+        <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+        <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      </section>
+    </a>
   `,
   styleUrls: ["./housing-location.component.scss"],
 })
